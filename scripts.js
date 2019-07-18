64a0281fa82d854a2c795f38f7c79486187e3759
Pavel <terror-billy@yandex.ru>
2019-07-18 19:43:07 +0300

    some more code

--- scripts.js
+++ scripts.js
@@ -90,3 +90,108 @@ Promise_All([soon(1), Promise.reject("X"),soon(3)]).then(array => {
 });
 
 /* ------------------------------------------------------------------------------------------------------------------- */
+/* Intermediate Algorithm Scripting: Search and Replace
+Perform a search and replace on the sentence using the arguments provided and return the new sentence. */
+function myReplace(str, before, after) {
+  let strArr = str.split(" ");
+  if(before.charAt(0) === before.charAt(0).toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.slice(1,);
+  }
+  strArr[strArr.indexOf(before)] = after;
+  return strArr.join(" ");
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "Jumped", "leaped");
+
+/* The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array. */
+function pairElement(str) {
+  let strArr = str.split("");
+  let DNA = [];
+  strArr.forEach(el => {
+      switch (el) {
+      case "C":
+        DNA.push([el,"G"]);
+        break;
+      case "G":
+        DNA.push([el,"C"]);
+        break;
+      case "A":
+        DNA.push([el,"T"]);
+        break;
+      case "T":
+        DNA.push([el,"A"]);
+        break;
+      }
+    });
+  return DNA;
+}
+
+pairElement("GCG");
+pairElement("TTGAG");
+/* DNA solution with objects & map()*/
+    function pairElement1(str) {
+    //create object for pair lookup
+    var pairs = {
+      "A": "T",
+      "T": "A",
+      "C": "G",
+      "G": "C"
+    };
+    //split string into array of characters
+    var arr = str.split("");
+    //map character to array of character and matching pair
+    return arr.map(x => [x,pairs[x]]);
+  }
+
+  //test here
+  pairElement("GCG");
+    /* --------------------------------------------------------------------------------------------------------------- */
+
+// Intermediate Algorithm Scripting: Missing letters
+// Find the missing letter in the passed letter range and return it.
+//
+// If all letters are present in the range, return undefined.
+// My solution
+function fearNotLetter(str) {
+    let strArr = str.split("");
+    let alphaRange = [];
+    let regExp = new RegExp('[' + str +']');
+    for(let i = 97; i <= 122; i++) {
+  	    alphaRange.push(i);
+    }
+    alphaRange = alphaRange.map(el => String.fromCharCode(el));
+    if(alphaRange.length === strArr.length) return undefined;
+    alphaRange.splice(alphaRange.indexOf(strArr[strArr.length - 1]),);
+    if(alphaRange[0] !== strArr[0]) {
+  	    alphaRange.splice(0,alphaRange.indexOf(strArr[0]));
+    }
+ 	str = alphaRange.filter(el => !regExp.test(el));
+ 	return str.join("");
+}
+
+fearNotLetter("abce");
+fearNotLetter("stvwx");
+//Better Solutions
+/* ---------------------- */
+function fearNotLetter1(str) {
+  for (let i = 1; i < str.length; ++i) {
+    if (str.charCodeAt(i) - str.charCodeAt(i-1) > 1) {
+      return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+    }
+  }
+}
+/* -------------------- */
+function fearNotLetter2(str) {
+  var compare = str.charCodeAt(0), missing;
+
+  str.split('').map(function(letter,index) {
+    if (str.charCodeAt(index) == compare) {
+      ++compare;
+    } else {
+      missing = String.fromCharCode(compare);
+    }
+  });
+
+  return missing;
+}
+
